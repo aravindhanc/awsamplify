ecf96688631a6011eee21f48f60b7ad91928f503
Aravindhan Chandrasekar <aravindhan.c@gmail.com>
2020-08-02 17:49:05 +1000

    dynamoDB added

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,11 @@ const awsmobile = {
     "aws_cognito_region": "ap-southeast-2",
     "aws_user_pools_id": "ap-southeast-2_8TYLGyvzP",
     "aws_user_pools_web_client_id": "2edibl0eob9l3vnc7q560imd8h",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://7s6tx7pnsvdwlpnsi4v4zw5mmu.appsync-api.ap-southeast-2.amazonaws.com/graphql",
+    "aws_appsync_region": "ap-southeast-2",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-47gorgfagnhtxdakfa5dskg7gi"
 };
 
 
